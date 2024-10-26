a71725f06a152c789e3eba48c879fb4bafff8b87
Nicholandn22 <nicholas.dwinata@ti.ukdw.ac.id>
2024-10-26 10:20:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -50,4 +50,6 @@ def index():
     return render_template('index2.html', data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
+    # app.run(debug=True)
